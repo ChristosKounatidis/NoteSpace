0460b90fc3b83c525c3d72c2214c63305fd1d4fb
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-10-21 19:29:39 +0300

    Merge branch 'main' of https://github.com/ChristosKounatidis/NoteSpace

--- DB/dbsetup.sql
+++ DB/dbsetup.sql
@@ -1,28 +1,28 @@
-DROP TABLE IF EXISTS Producer;
-CREATE TABLE IF NOT EXISTS Producer(
-	id INT NOT NULL,
-	name VARCHAR(15),
-	PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS Artist;
-CREATE TABLE IF NOT EXISTS Artist(
-	id INT NOT NULL,
-	name VARCHAR(15),
-	category INT,	
-	PRIMARY KEY (id)
-);
-
-DROP TABLE IF EXISTS Categories;
-CREATE TABLE IF NOT EXISTS Categories(
-	id INT NOT NULL,
-	category VARCHAR(15),
-	PRIMARY KEY (id)
-);
-
+DROP TABLE IF EXISTS Track;
 DROP TABLE IF EXISTS Album;
-CREATE TABLE IF NOT EXISTS Album(
-	id INT NOT NULL,
+DROP TABLE IF EXISTS songArtist;
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Producer;
+
+CREATE TABLE Producer(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(15)
+);
+
+CREATE TABLE Artist(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(15),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Categories(
+	id SERIAL PRIMARY KEY,
+	category VARCHAR(15)
+);
+
+CREATE TABLE Album(
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(30),
 	category INT,
 	releasedata DATE,
@@ -33,8 +33,7 @@ CREATE TABLE IF NOT EXISTS Album(
 			REFERENCES Artist(id)
 );
 
-DROP TABLE IF EXISTS Track;
-CREATE TABLE IF NOT EXISTS Track(
+CREATE TABLE Track(
 	id INT NOT NULL,
 	name VARCHAR(30),
 	producerid INT,
@@ -52,8 +51,7 @@ CREATE TABLE IF NOT EXISTS Track(
 			REFERENCES Categories(id)
 );
 
-DROP TABLE IF EXISTS songArtist;
-CREATE TABLE IF NOT EXISTS songArtist(
+CREATE TABLE songArtist(
     category INT NOT NULL,
     artist INT NOT NULL,
     PRIMARY KEY (category,artist),
@@ -64,5 +62,3 @@ CREATE TABLE IF NOT EXISTS songArtist(
 		FOREIGN KEY(artist) 
 			REFERENCES Artist(id)
 );
-
-
