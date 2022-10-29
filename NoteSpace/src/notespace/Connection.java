4535512d7887c30bf53302170c470c72dcf87c7e
ChristosKounatidis <kounax0@gmail.com>
2022-10-30 00:19:51 +0300

    Update Connection.java
    login credentials v01

--- NoteSpace/src/notespace/Connection.java
+++ NoteSpace/src/notespace/Connection.java
@@ -4,12 +4,13 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class Connection {
-        
-    static String     driverClassName = "org.postgresql.Driver" ;
-    static String     url = "jdbc:postgresql://dblabs.it.teithe.gr:5432/it174940" ;
+    Credentials login = new Credentials();
+    
+    static String driverClassName = "org.postgresql.Driver" ;
+    static String url = "jdbc:postgresql://dblabs.it.teithe.gr:5432/it174940" ;
     static java.sql.Connection dbConnection = null;
-    static String     username = "it174940";
-    static String     passwd = "4Kz_hxTw";
+    String username = login.getUsername();
+    String passwd = login.getPasswd();
     static Statement  statement = null;
 
 }
