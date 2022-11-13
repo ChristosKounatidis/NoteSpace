86059d445a86b4a911b6806d3ad50c76cd0c35a4
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-13 19:44:50 +0200

    Data insertion function

---
+++ DB/data.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE OR REPLACE FUNCTION new_song(character varying)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    INSERT INTO Song(name) VALUES($1);
+    END;
+$function$;
+select new_song('Lobby');
+select * from Song;
