db5e1866fcacf4c6a48e6a4f3e58097fe1a5750f
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-13 19:53:32 +0200

    More functions

--- DB/data.sql
+++ DB/data.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE OR REPLACE FUNCTION new_song(character varying)
+﻿CREATE OR REPLACE FUNCTION new_song(character varying)
  RETURNS void
  LANGUAGE plpgsql
 AS $function$
@@ -7,5 +6,44 @@ AS $function$
     INSERT INTO Song(name) VALUES($1);
     END;
 $function$;
+
+CREATE OR REPLACE FUNCTION new_Artist(character varying)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    INSERT INTO Artist(name) VALUES($1);
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION new_Album(character varying,date)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    INSERT INTO Album(name,releasedate) VALUES($1,$2);
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION new_Producer(character varying)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    INSERT INTO Producer(name) VALUES($1);
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION new_Category(character varying)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    INSERT INTO Categories(name) VALUES($1);
+    END;
+$function$;
+
+
+
 select new_song('Lobby');
 select * from Song;
