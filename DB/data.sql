9920b296e9b36d2772cd23875f06e7fd357b86fb
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-21 19:55:39 +0200

    Update data.sql

--- DB/data.sql
+++ DB/data.sql
@@ -6,3 +6,21 @@ AS $function$
     insert into Producer(name) values ($1);
     END;
 $function$;
+
+CREATE OR REPLACE FUNCTION new_Artist(artist_name VARCHAR(15))
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    insert into Artist(name) values ($1);
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION new_Song(song_name VARCHAR(30),producer_name VARCHAR(15),category VARCHAR(15))
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    insert into Song(name,producer,category) values ($1,$2,$3);
+    END;
+$function$;
