5f0a59e30595bc801a3bee1f9f8958dd90088c63
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-21 21:14:39 +0200

    Update data.sql

--- DB/data.sql
+++ DB/data.sql
@@ -52,40 +52,66 @@ AS $function$
     END;
 $function$;
 
-select new_Producer(Tillman Gyles);
-select new_Producer(Akeem Mcilvenny);
-select new_Producer(Ainsleigh Kluss);
-select new_Producer(Elimelech Olvey);
-select new_Producer(Alyn Seider);
+CREATE OR REPLACE FUNCTION producers()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    select new_Producer(Tillman Gyles);
+    select new_Producer(Akeem Mcilvenny);
+    select new_Producer(Ainsleigh Kluss);
+    select new_Producer(Elimelech Olvey);
+    select new_Producer(Alyn Seider);
+    END;
+$function$;
 
-select new_Artist(Edvard Bacon);
-select new_Artist(Wassily Kandinsky);
-select new_Artist(Claude Picasso);
-select new_Artist(Georges Rodin);
-select new_Artist(Keith Basquiat);
+CREATE OR REPLACE FUNCTION artists()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    select new_Artist(Edvard Bacon);
+    select new_Artist(Wassily Kandinsky);
+    select new_Artist(Claude Picasso);
+    select new_Artist(Georges Rodin);
+    select new_Artist(Keith Basquiat);
+    END;
+$function$;
 
-select new_Album(Spilled beans);
-select new_Album(Nine yards);
-select new_Album(Honesty);
-select new_Album(Impulse reaction);
-select new_Album(Ghost stories);
+CREATE OR REPLACE FUNCTION albums()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    select new_Album(Spilled beans);
+    select new_Album(Nine yards);
+    select new_Album(Honesty);
+    select new_Album(Impulse reaction);
+    select new_Album(Ghost stories);
+    END;
+$function$;
 
-select new_Song(White Horse);
-select new_Song(cheery wings);
-select new_Song(Little Lion Man);
-select new_Song(legendary YOUTH);
-select new_Song(eez-eh);
-select new_Song(All My Grenade);
-select new_Song(Retro june);
-select new_Song(teen Flashback);
-select new_Song(easy Grenade);
-select new_Song(rubber Kindness);
-select new_Song(Burn queen);
-select new_Song(Often Motion);
-select new_Song(Alternative);
-select new_Song(The Reason);
-select new_Song(Happier Angel);
-select new_Song(The Same);
+CREATE OR REPLACE FUNCTION songs()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    select new_Song(White Horse);
+    select new_Song(cheery wings);
+    select new_Song(Little Lion Man);
+    select new_Song(legendary YOUTH);
+    select new_Song(eez-eh);
+    select new_Song(All My Grenade);
+    select new_Song(Retro june);
+    select new_Song(teen Flashback);
+    select new_Song(easy Grenade);
+    select new_Song(rubber Kindness);
+    select new_Song(Burn queen);
+    select new_Song(Often Motion);
+    select new_Song(Alternative);
+    select new_Song(The Reason);
+    select new_Song(Happier Angel);
+    select new_Song(The Same);
+$function$;
 
 
 
