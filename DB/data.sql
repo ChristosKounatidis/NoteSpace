12b40a57be9eb68c7cdf3d872bb342aac87d66e7
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-21 20:32:17 +0200

    Update data.sql

--- DB/data.sql
+++ DB/data.sql
@@ -21,11 +21,9 @@ CREATE OR REPLACE FUNCTION new_Album(album_name VARCHAR(30),releasedate DATE,art
  LANGUAGE plpgsql
 AS $function$
     BEGIN
-    insert into Song(name,producer,album,category) values ($1,$2,$3,$4);
+    insert into Album(name,releasedate,artist) values ($1,$2,$3);
     END;
 $function$;
-
-
 
 CREATE OR REPLACE FUNCTION new_Song(song_name int,producer_name int,album int,category VARCHAR(15))
  RETURNS void
@@ -33,6 +31,24 @@ CREATE OR REPLACE FUNCTION new_Song(song_name int,producer_name int,album int,ca
 AS $function$
     BEGIN
     insert into Song(name,producer,album,category) values ($1,$2,$3,$4);
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION new_Artist_Song(song_name int,producer_name int)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    insert into Artist_Song(artist,song) values ($1,$2);
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION new_Artist_Song(Username VARCHAR(20),pass VARCHAR(20))
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    insert into Users(username,pwd) values ($1,$2);
     END;
 $function$;
 
