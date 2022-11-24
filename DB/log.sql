76b9391037e77f394e3ccc4812fd57d4eac7e66f
Stratos Skarlatos <40740528+Haxemaniac@users.noreply.github.com>
2022-11-25 00:42:50 +0200

    log tables for db

--- DB/log.sql
+++ DB/log.sql
@@ -1,13 +1,106 @@
-CREATE OR REPLACE new_log_file()
+CREATE OR REPLACE FUNCTION create_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION create_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION create_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION create_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION create_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION create_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION createlog () 
 RETURNS TRIGGER AS $$
 BEGIN
-    CREATE IF EXISTS TABLE log_file(
-        operation char(1) not null,
-         timestamp not null ,
-
-    )
+IF (TG_OP = 'DELETE') THEN
+INSERT INTO Log SELECT 'D', now();
+RETURN OLD;
+ELSIF (TG_OP = 'UPDATE') THEN
+INSERT INTO Log SELECT 'U', now();
+RETURN NEW;
+ELSIF (TG_OP = 'INSERT') THEN
+INSERT INTO Log SELECT 'I', now();
+RETURN NEW;
+END IF;
+RETURN NULL; 
 END;
 $$ 
 LANGUAGE plpgsql;
 
+CREATE TRIGGER keeplog
+AFTER INSERT OR UPDATE OR DELETE ON Log
+FOR EACH ROW EXECUTE PROCEDURE createlog();
 
