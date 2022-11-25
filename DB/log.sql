83c614469c7168f03433b841b209e147524e141e
Stratos Skarlatos <40740528+Haxemaniac@users.noreply.github.com>
2022-11-25 15:03:35 +0200

    Log triggers for all tables

--- DB/log.sql
+++ DB/log.sql
@@ -1,4 +1,60 @@
-CREATE OR REPLACE FUNCTION create_log()
+CREATE OR REPLACE FUNCTION artist_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS artist_log;
+    CREATE TABLE artist_log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION producer_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION album_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION song_log()
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    DROP TABLE if EXISTS Log;
+    CREATE TABLE Log(
+	method enum('d','u','i'),
+	table_name	VARCHAR(20),
+	moment timestamp
+    );
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION user_log()
  RETURNS void
  LANGUAGE plpgsql
 AS $function$
@@ -20,87 +76,57 @@ AS $function$
     DROP TABLE if EXISTS Log;
     CREATE TABLE Log(
 	method enum('d','u','i'),
-	table_name	VARCHAR(20),
+    old_data VARCHAR(100),
+    new_data VARCHAR(100),
+	table_name	VARCHAR(10),
 	moment timestamp
     );
     END;
 $function$;
 
-CREATE OR REPLACE FUNCTION create_log()
- RETURNS void
- LANGUAGE plpgsql
-AS $function$
-    BEGIN
-    DROP TABLE if EXISTS Log;
-    CREATE TABLE Log(
-	method enum('d','u','i'),
-	table_name	VARCHAR(20),
-	moment timestamp
-    );
-    END;
-$function$;
+CREATE or REPLACE FUNCTION logger() RETURNS trigger AS $$
+        BEGIN
+        IF TG_OP = 'DELETE'
+        THEN
+           INSERT INTO Log(method,old_data,new_data,table_name,moment)
+            VALUES ('d',(select old::text),'',TG_TABLE_NAME,now());
+            RETURN OLD;            
+        ELSIF TG_OP = 'INSERT'
+        THEN
+           INSERT INTO Log(method,old_data,new_data,table_name,moment)
+            VALUES ('i','',(select new::text),TG_TABLE_NAME,now());
+            RETURN NEW;
+        ELSIF  TG_OP = 'UPDATE'
+        THEN
+            INSERT INTO Log(method,old_data,new_data,table_name,moment)
+            VALUES ('u',(select old::text),(select new::text),TG_TABLE_NAME,now());
+            RETURN NEW;
+            END IF;
+        END;
+$$ LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION create_log()
- RETURNS void
- LANGUAGE plpgsql
-AS $function$
-    BEGIN
-    DROP TABLE if EXISTS Log;
-    CREATE TABLE Log(
-	method enum('d','u','i'),
-	table_name	VARCHAR(20),
-	moment timestamp
-    );
-    END;
-$function$;
 
-CREATE OR REPLACE FUNCTION create_log()
- RETURNS void
- LANGUAGE plpgsql
-AS $function$
-    BEGIN
-    DROP TABLE if EXISTS Log;
-    CREATE TABLE Log(
-	method enum('d','u','i'),
-	table_name	VARCHAR(20),
-	moment timestamp
-    );
-    END;
-$function$;
+CREATE TRIGGER log_producer 
+AFTER INSERT OR UPDATE OR DELETE ON Producer
+FOR EACH ROW EXECUTE PROCEDURE logger();
 
-CREATE OR REPLACE FUNCTION create_log()
- RETURNS void
- LANGUAGE plpgsql
-AS $function$
-    BEGIN
-    DROP TABLE if EXISTS Log;
-    CREATE TABLE Log(
-	method enum('d','u','i'),
-	table_name	VARCHAR(20),
-	moment timestamp
-    );
-    END;
-$function$;
+CREATE TRIGGER log_artist 
+AFTER INSERT OR UPDATE OR DELETE ON Artist
+FOR EACH ROW EXECUTE PROCEDURE logger();
 
-CREATE OR REPLACE FUNCTION createlog () 
-RETURNS TRIGGER AS $$
-BEGIN
-IF (TG_OP = 'DELETE') THEN
-INSERT INTO Log SELECT 'D', now();
-RETURN OLD;
-ELSIF (TG_OP = 'UPDATE') THEN
-INSERT INTO Log SELECT 'U', now();
-RETURN NEW;
-ELSIF (TG_OP = 'INSERT') THEN
-INSERT INTO Log SELECT 'I', now();
-RETURN NEW;
-END IF;
-RETURN NULL; 
-END;
-$$ 
-LANGUAGE plpgsql;
+CREATE TRIGGER log_album 
+AFTER INSERT OR UPDATE OR DELETE ON Album
+FOR EACH ROW EXECUTE PROCEDURE logger();
 
-CREATE TRIGGER keeplog
-AFTER INSERT OR UPDATE OR DELETE ON Log
-FOR EACH ROW EXECUTE PROCEDURE createlog();
+CREATE TRIGGER log_song 
+AFTER INSERT OR UPDATE OR DELETE ON Song
+FOR EACH ROW EXECUTE PROCEDURE logger();
+
+CREATE TRIGGER log_artist_song
+AFTER INSERT OR UPDATE OR DELETE ON Artist_Song
+FOR EACH ROW EXECUTE PROCEDURE logger();
+
+CREATE TRIGGER log_users
+AFTER INSERT OR UPDATE OR DELETE ON Users
+FOR EACH ROW EXECUTE PROCEDURE logger();
 
