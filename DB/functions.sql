f42fb5dffcb7e5ee414ce1d853c9d286740c4a44
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2022-11-26 19:25:18 +0200

    Update functions.sql

--- DB/functions.sql
+++ DB/functions.sql
@@ -12,3 +12,46 @@ AS $function$
 	END IF;
     END;
 $function$;
+
+
+----------------------------------------------------------
+--search functions for data
+
+CREATE OR REPLACE FUNCTION search_artist(artist_name VARCHAR(30))
+ RETURNS Boolean
+ LANGUAGE plpgsql
+AS $function$
+DECLARE search_key VARCHAR(40);
+    BEGIN
+	search_key := CONCAT('%',artist_name,'%');
+    SELECT * FROM Artist WHERE name IS LIKE search_key;
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION search_producer()
+ RETURNS Boolean
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION search_album()
+ RETURNS Boolean
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    
+    END;
+$function$;
+
+CREATE OR REPLACE FUNCTION search_song()
+ RETURNS Boolean
+ LANGUAGE plpgsql
+AS $function$
+    BEGIN
+    
+    END;
+$function$;
+
