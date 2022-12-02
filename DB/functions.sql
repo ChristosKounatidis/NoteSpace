577e9015f686795490f79a2351f121cf88f81be9
Stratos Skarlatos <40740528+Haxemaniac@users.noreply.github.com>
2022-12-02 21:42:44 +0200

    fix search_song

--- DB/functions.sql
+++ DB/functions.sql
@@ -93,19 +93,19 @@ DECLARE
     BEGIN
     --by song name (ψάχνει και με κομμάτι του ον´οματος )
     IF  search_method='by_name' THEN
-    temp1 := perform search_item(search_key,'Song');
+    temp := perform search_item(search_key,'Song');
     RETURN QUERY SELECT s.name from Song s WHERE category=temp;
     --by album (πρέπει το όνομα να ε´ιναι ακριβώς )
     ELSIF search_method='by_album' THEN
-    temp1 := perform search_item(search_key,'Album');
+    temp := perform search_item(search_key,'Album');
     RETURN QUERY SELECT s.name from Song s WHERE album=temp;
     --by producer (´οπως το πάνω)
     ELSIF search_method='by_producer' THEN
-    temp1 := perform search_item(search_key,'Producer');
+    temp := perform search_item(search_key,'Producer');
     RETURN QUERY SELECT s.name from Song s WHERE producer=temp;
     --by artist  (όπως το πάνω)
     ELSIF search_method='by_artist' THEN
-    temp1 := perform search_item(search_key,'Artist');
+    temp := perform search_item(search_key,'Artist');
     RETURN QUERY SELECT s.name from Song s WHERE artist=temp;
     --by category (η κατηγορία πρέπει να γραφτεί ακρ´ιβως ,combobox)
     ELSIF search_method='by_category' THEN
